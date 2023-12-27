648f1931808f294dc3b0b7bb2f40972a85154a5f
Thiago <thiagogoes1011@gmail.com>
2023-12-27 11:19:35 -0300

    irei mudar de branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import useTimer from "./useTimerHook"
+
 
 function App() {
+
+  const [state, dispatch] = useTimer();
+  const ref = useRef();
+
+  if(state.minuto == 0 && state.segundo == 0){
+    clearInterval(ref.current);
+    console.log("chamando intervalo" + ref.current);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding: "40px"}}>
+       <p>{state.timer}</p>
+       <button onClick={() =>{
+         ref.current = setInterval(() => {
+          dispatch({ type: "decremento" });       
+         }, 1000);
+       }}> Iniciar</button>
+       <button onClick={() =>{
+        clearInterval(ref.current);
+       }}>pausar</button>
+
+       <button onClick={()=>{
+        clearInterval(ref.current);
+        dispatch({ type: "resetar" });
+       }}>reset</button>
+       <button onClick={()=>{
+        const campoM = document.getElementById("minuto");
+        const campoS = document.getElementById("segundo");
+          dispatch({ type: "editar",
+            campos: [campoM.value, campoS.value] });
+       }}>edit</button>
+       <label>minuto</label>
+       <input type="number" id="minuto"/>
+       <label>segundo</label>
+       <input type="number" id="segundo"/>
     </div>
   );
 }
---
+++ src/useTimerHook.js
@@ -0,0 +1,57 @@
+import { useReducer } from "react";
+
+
+let InicialState = {timer: "00:30" , minuto: 0, segundo: 30};
+
+
+function reducer(state, action){
+    console.log(action)
+    switch(action.type){
+        case "decremento" :{
+            const minuto = state.minuto;
+            const segundo = state.segundo;
+            
+            if(segundo != 0){
+                return {
+                    timer: minuto.toString().padStart(2, 0) + ":" + (segundo - 1).toString().padStart(2, 0),
+                    minuto: minuto,
+                    segundo: segundo - 1
+                }
+            }else if(minuto != 0){
+                return {
+                    timer: (minuto - 1).toString().padStart(2, 0) + ":" + 59 ,
+                    minuto: minuto - 1,
+                    segundo: 59
+                } 
+            }else{
+                return {
+                    timer: "00:00" ,
+                    minuto: 0,
+                    segundo: 0
+                } 
+            }       
+        }
+        case "resetar":{
+            return InicialState;
+        }
+        case "editar":{
+            
+            const objeto = {
+                timer: (action.campos[0] ).toString().padStart(2, 0) + ":" + (action.campos[1]).toString().padStart(2, 0) ,
+                minuto: action.campos[0],
+                segundo: action.campos[1]
+            };
+            InicialState = objeto;
+            return objeto;
+        }
+    }
+
+}
+
+
+
+export default function useTimer(){
+    const [state, dispatch] = useReducer(reducer, InicialState);
+
+    return [state, dispatch];
+} 
